72535326ab85a474b1e880293b89620db3122e87
Nguyen Anh Thanh <Thanhanh.Nguyen@isobar.com>
2020-07-27 19:06:37 +0700

    first commit

--- server/src/config/vars.js
+++ server/src/config/vars.js
@@ -4,6 +4,7 @@ const path = require('path');
 require('dotenv-safe').load({
   path: path.join(__dirname, '../../.env'),
   sample: path.join(__dirname, '../../.env.example'),
+  allowEmptyValues: true
 });
 
 module.exports = {
